9198c23c998aef16819b6a8b72a5a37d90eb2bde
Dmitriy Non <non.dmitriy@gmail.com>
2017-10-29 12:42:32 +0300

    add ring

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.6.2"]
+                 [ring/ring-jetty-adapter "1.5.0"]]
   :main ^:skip-aot everine.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler everine.core/handler
+         :open-browser? false})
