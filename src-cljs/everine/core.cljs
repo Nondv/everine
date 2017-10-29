6ee516f7649cc742745abe6a22db48ab3678d8ec
Dmitriy Non <non.dmitriy@gmail.com>
2017-10-29 23:27:27 +0300

    delete some useless code from cljs everine.core

--- src-cljs/everine/core.cljs
+++ src-cljs/everine/core.cljs
@@ -7,15 +7,9 @@
 (println "Hello World!")
 (println "This is ClojureScript!")
 
-(rum/defc label [text]
-   [:div {:class "label"} text])
-
-
 (def items [{:id 1 :label "item1"} {:id 2 :label "item2"} {:id 3 :label "item3"}])
-(def items-atom (atom items))
 
 (rum/defcs app < (rum/local items ::items) [state]
-  [:div [(rum/with-key (label "Yarrr!!") 1)
-         (rum/with-key (todo-list (::items state)) 2)]])
+  (todo-list (::items state)))
 
 (rum/mount (app) (js/document.getElementById "app" ))
