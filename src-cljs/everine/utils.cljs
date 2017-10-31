6dbbf84f39717f118778526d60631f1b105eed78
Dmitriy Non <non.dmitriy@gmail.com>
2017-11-01 02:22:15 +0300

    [cljs] add find-by function to utils

--- src-cljs/everine/utils.cljs
+++ src-cljs/everine/utils.cljs
@@ -4,3 +4,10 @@
   "Returns xs with f applied to elements with key = name"
   [key value f xs]
   (map #(if (= value (key %)) (f %) %) xs))
+
+(defn find-by
+  "Returns first map of `maps` with `key` = `value`"
+  [key value maps]
+  (first (filter #(= value (key %)) maps)))
+
+(defn log [data] (js/console.log (str data)))
