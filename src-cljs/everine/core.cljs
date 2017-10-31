f0f54a4a788792f0655c23e104a3a890f02f3e6c
Dmitriy Non <non.dmitriy@gmail.com>
2017-11-01 02:27:15 +0300

    [cljs] add multiple lists to app

--- src-cljs/everine/core.cljs
+++ src-cljs/everine/core.cljs
@@ -1,6 +1,7 @@
 (ns everine.core
   (:use [everine.components.todo-list :only [todo-list]])
-  (:require [rum.core :as rum]))
+  (:require [rum.core :as rum]
+            [everine.utils :as utils]))
 
 (enable-console-print!)
 
@@ -8,9 +9,26 @@
 (println "This is ClojureScript!")
 
 (def items [{:id 1 :label "item1"} {:id 2 :label "item2"} {:id 3 :label "item3"}])
+(def lists [{:name "test-list"  :items items}
+            {:name "empty-list" :items []}])
 
-(rum/defcs app < (rum/local items ::items) [state]
-  (let [items-atom (::items state)]
-    (todo-list @items-atom #(reset! items-atom %))))
+(def update-by-name (partial utils/update-by :name))
+(defn replace-by-name [name data maps]
+  (update-by-name name (fn [_] data) maps))
+(def find-by-name (partial utils/find-by :name))
+
+(defn replace-items [data list] (assoc list :items data))
+
+(rum/defcs app <
+  (rum/local lists ::lists)
+  (rum/local "test-list" ::current-list-name)
+  [state]
+
+  (let [lists (::lists state)
+        current-list-name (::current-list-name state)
+        current-list (find-by-name @current-list-name @lists)
+        replace-current-list-items #(replace-by-name @current-list-name (replace-items % current-list) @lists)
+        dispatch #(reset! lists (replace-current-list-items %))]
+    (todo-list (:items current-list) dispatch)))
 
 (rum/mount (app) (js/document.getElementById "app" ))
