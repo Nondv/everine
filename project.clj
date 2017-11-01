c55651ef308ec4ea5ac3543236085a1f13fd6a1c
Dmitriy Non <non.dmitriy@gmail.com>
2017-11-02 01:22:11 +0300

    [dev] disable cljs hooks

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
             [cider/cider-nrepl "0.16.0-SNAPSHOT"]
             [lein-cljsbuild "1.1.7"]]
   :aliases {"hrepl" ["repl" ":headless" ":host" "0.0.0.0" ":port" "7888"]}
-  :hooks [leiningen.cljsbuild]
+  ;; :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds
               [{:id "cljsbuild" ;; name for a build
                 :source-paths ["src-cljs"]
