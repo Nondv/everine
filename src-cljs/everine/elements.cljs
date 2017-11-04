855c129bb09e370cce38430bdb2f146cc2ca609b
Dmitriy Non <non.dmitriy@gmail.com>
2017-11-04 15:34:16 +0300

    [cljs] add Material Design Lite and use it for submit buttons

--- src-cljs/everine/elements.cljs
+++ src-cljs/everine/elements.cljs
@@ -1,13 +1,22 @@
 (ns everine.elements
-    (:require [everine.utils :as utils]))
+  (:require [clojure.string :as string]
+            [everine.utils :as utils]))
 
 (defn text-input [value class on-change]
   [:input {:type :text
            :value value
            :on-change #(on-change (utils/event-target-value %))
            :class class} ])
+(def default-button-class "mdl-button mdl-js-button mdl-button--raised mdl-button--colored")
+
+
+(defn button-class
+  "Generates string for `class=` attribute"
+  [& additionals]
+  (str default-button-class " " (string/join " " additionals)))
+
 
 (defn submit-button [value class]
   [:input {:type :submit
            :value value
-           :class class}])
+           :class (button-class class)}])
